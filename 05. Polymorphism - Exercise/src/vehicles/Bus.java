58f53328bff8a771e7117919825c4d0fab04217b
rborisovv <radii2000@abv.bg>
2021-11-13 17:19:29 +0200

    Removed switch statements in the drive method by adding additional parameter in the declaration

---
+++ 05. Polymorphism - Exercise/src/vehicles/Bus.java
@@ -0,0 +1,33 @@
+package vehicles;
+
+import java.text.DecimalFormat;
+
+public class Bus extends VehicleImpl {
+    static final double BUSS_INCREASED_CONSUMPTION = 1.4;
+
+    public Bus(double fuelQuantity, double fuelConsumption, double tankCapacity) {
+        super(fuelQuantity, fuelConsumption, tankCapacity);
+    }
+
+    public String driveEmpty(double kilometers) {
+        double usedFuel = kilometers * this.getFuelConsumption();
+        double remainingFuel = this.getFuelQuantity() - usedFuel;
+        String className = this.getClass().getSimpleName();
+        if (remainingFuel >= 0) {
+            this.setFuelQuantity(this.getFuelQuantity() - usedFuel);
+        } else {
+            return VehicleImpl.VEHICLE_NEEDS_REFUELING_MSG.apply(className);
+        }
+        DecimalFormat formatter = new DecimalFormat("##.##");
+        return VehicleImpl.VEHICLE_DRIVEN_MSG.apply(className, formatter.format(kilometers));
+    }
+
+    @Override
+    public void refuel(double liters) {
+        if (liters <= 0) {
+            System.out.println("Fuel must be a positive number");
+            return;
+        }
+        this.setFuelQuantity(getFuelQuantity() + liters);
+    }
+}
--- 05. Polymorphism - Exercise/src/vehicles/Car.java
+++ 05. Polymorphism - Exercise/src/vehicles/Car.java
@@ -3,12 +3,16 @@ package vehicles;
 public class Car extends VehicleImpl {
     static final double FUEL_CONSUMPTION_SUMMER_INCREASE = 0.9;
 
-    public Car(double fuelQuantity, double fuelConsumption) {
-        super(fuelQuantity, fuelConsumption);
+    public Car(double fuelQuantity, double fuelConsumption, double tankCapacity) {
+        super(fuelQuantity, fuelConsumption, tankCapacity);
     }
 
     @Override
     public void refuel(double liters) {
+        if (liters <= 0) {
+            System.out.println("Fuel must be a positive number");
+            return;
+        }
         setFuelQuantity(getFuelQuantity() + liters);
     }
 }
