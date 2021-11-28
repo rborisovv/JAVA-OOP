274caffa0f3442a3f9430193c6abe75d8c0b02fe
rborisovv <radii2000@abv.bg>
2021-11-28 21:17:14 +0200

    Added tests covering sixth exercise, using Mockito

--- 08. Unit Testing - Exercise/src/main/java/p06_TirePressureMonitoringSystem/Alarm.java
+++ 08. Unit Testing - Exercise/src/main/java/p06_TirePressureMonitoringSystem/Alarm.java
@@ -4,7 +4,7 @@ public class Alarm {
     private static final double LOW_PRESSURE_THRESHOLD = 17;
     private static final double HIGH_PRESSURE_THRESHOLD = 21;
 
-    private Sensor sensor;
+    private final Sensor sensor;
 
     private boolean alarmOn = false;
 
